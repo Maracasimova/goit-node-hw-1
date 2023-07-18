2b5d12fc1399bdaee4f53903f851b68560605ae2
Casimova Marina <maracasimova@gmail.com>
2023-07-18 18:33:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-} = require("./contacts");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const nodemon = require("nodemon");
 
-const argv = require('yargs').argv;
+const contacts = require("./contacts");
 
-function invokeAction({ action, id, name, email, phone }) {
+const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case 'list':
-      listContacts();
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
       break;
 
-    case 'get':
-       getContactById(id);
+    case "get":
+      const getByID = await contacts.getContactById(id);
+      console.log(getByID);
       break;
 
-    case 'add':
-      addContact(name, email, phone);
+    case "add":
+      const addNewContact = await contacts.addContact(name, email, phone);
+      console.log(addNewContact);
       break;
 
-    case 'remove':
-       removeContact(id);
+    case "remove":
+      const removeByID = await contacts.removeContact(id);
+      console.log(removeByID);
       break;
 
     default:
-      console.warn('\x1B[31m Unknown action type!');
+      console.warn("\x1B[31m Unknown action type!");
   }
-}
+};
 
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
 invokeAction(argv);
